b5dc62b924913ae528281f9eda81a77098477d7f
bberst <32759956+bberst@users.noreply.github.com>
2020-01-10 16:37:35 -0800

    removed explicit db references

--- production.tf
+++ production.tf
@@ -26,18 +26,6 @@ module "networking" {
   key_name             = "production_key"
 }
 
-module "rds" {
-  source            = "./modules/rds"
-  environment       = "production"
-  allocated_storage = "20"
-  database_name     = var.production_database_name
-  database_username = var.production_database_username
-  database_password = var.production_database_password
-  subnet_ids        = module.networking.private_subnets_id
-  vpc_id            = module.networking.vpc_id
-  instance_class    = "db.t2.micro"
-}
-
 module "ecs" {
   source             = "./modules/ecs"
   environment        = "production"
@@ -46,14 +34,7 @@ module "ecs" {
   repository_name    = "openjobs/production"
   subnets_ids        = module.networking.private_subnets_id
   public_subnet_ids  = module.networking.public_subnets_id
-  security_groups_ids = [
-    module.networking.security_groups_ids,
-    module.rds.db_access_sg_id,
-  ]
-  database_endpoint = module.rds.rds_address
-  database_name     = var.production_database_name
-  database_username = var.production_database_username
-  database_password = var.production_database_password
+  security_groups_ids = [module.networking.security_groups_ids]
   secret_key_base   = var.production_secret_key_base
 }
 
